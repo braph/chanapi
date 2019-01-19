0d38386f7e66f7b58e86c5eeb93b1b4b42d759ad
braph <braph93@gmx.de>
2019-01-19 17:22:38 +0100

    update

---
+++ chanapi/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+from chanapi.chanapi import \
+    ChanUpload, ChanJson, FileInfo
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chanapi",
+    version="0.0.1",
+    author="Benjamin Abendroth",
+    author_email="braph93@gmx.de",
+    description="api for imageboards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/braph/chanapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
